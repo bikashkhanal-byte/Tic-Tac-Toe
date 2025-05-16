02d8af7b481deb64dbb11c5dc39c9324e74b2f0f
Bikash Khanal <bikashkhanak760@gmail.com>
2025-05-16 14:48:31 +0545

    created room

--- server/index.js
+++ server/index.js
@@ -1,26 +1,63 @@
 
 
   const express = require('express')
+  const cors = require('cors')
+  const crypto = require('crypto')
+
   const app = express()
   const port = 3000
 
-  const db = [
-      {
-        name: "Joe",
-        roll: 1
-    },
-    {
-        name: "Moe",
-        roll: 2
+
+  app.use(cors());
+  app.use(express.json());
+
+  const rooms = {}
+
+  //room created 
+  app.post('/create-room', (req, res) => {
+    const roomId = crypto.randomBytes(16).toString('hex');
+    const player1Id = crypto.randomBytes(16).toString('hex');
+  
+    if (rooms[roomId]) {
+      return res.status(400).send({ message: 'Room already exists' });
     }
-]
+    rooms[roomId] = {
+      player1Id,
+      player2Id: null
+    };
+    
+    res.send({
+       message: 'Room created successfully',
+       roomId: roomId
+      
+      });
+  });
+  
+  //player 2 joins toom
 
-app.get('/', (req, res) => {
-console.log("Hello world");
-  res.json(db[0])
+  app.post('/join-room', (req, res) => {
+  
+    const roomId = crypto.randomBytes(16).toString('hex');
+    const player2Id = crypto.randomBytes(16).toString('hex');
 
-})
+    if (rooms[roomId]) {
+      return res.status(400).send({ message: 'Room already exists' });
+    }
+  
+    if(roomId.player2Id){
+      return res.status(400).send({ message: 'Room already has player 2 ' });
+      }
 
-app.listen(port , "192.168.1.12", () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    roomId.player2Id = player2Id;
+    rooms[roomId] = {
+    player1Id,
+      player2Id,
+    };
+  
+    res.send({ message: 'Room created successfully' });
+  });
+  
+  
+  app.listen(port , () =>{
+    console.log(`server running on https://${port}`);
+  })
